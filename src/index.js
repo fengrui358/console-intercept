52bb42742052fe2fd0a8a777df670460974dfd88
fengrui <fengrui358@163.com>
2021-10-09 11:42:22 +0800

    add interceptRemoteLog

--- src/index.js
+++ src/index.js
@@ -11,20 +11,52 @@ export const intercept = function (fun) {
 
         window.console = (function (origConsole) {
             let result = {};
-    
+
             Object.values(console)
                 .filter((s) => typeof s === 'function')
                 .forEach((f) => {
                     result[f.name] = function () {
                         let args = [...arguments];
                         origConsole[f.name].apply(origConsole, args);
-                        interceptFuns.forEach(fun => {
+                        interceptFuns.forEach((fun) => {
                             fun(f.name, [...arguments]);
                         });
                     };
                 });
-    
+
             return result;
         })(window.console);
     }
+};
+
+export const interceptRemoteLog = function (appName, url) {
+    intercept(function (name, args) {
+        let request = {
+            appName: appName,
+            logLevel: name,
+            logTime: new Date(),
+        };
+
+        let messages = [];
+        args.forEach((m) => {
+            if (m != null) {
+                if (typeof m === 'string') {
+                    messages.push(m);
+                } else if (m instanceof Error) {
+                    messages.push(
+                        JSON.stringify(m, Object.getOwnPropertyNames(m))
+                    );
+                } else if (typeof m === 'object') {
+                    messages.push(JSON.stringify(m));
+                } else {
+                    messages.push(m.toString());
+                }
+            }
+        });
+
+        let req = new XMLHttpRequest();
+        req.open('POST', url, true);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.send(JSON.stringify(request));
+    });
 };
